726d3d585e796476130d0ad80f02ddb1a4b99f66
muhammadqamar111 <muhammadqamar111@gmail.com>
2023-08-24 18:00:54 +0500

    make anyltics api dynamic

--- src/analytics/analytics.controller.ts
+++ src/analytics/analytics.controller.ts
@@ -1,18 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import axios from 'axios';
 
 @Controller('analytics')
 export class AnalyticsController {
   private readonly googleAnalyticsUrl =
     'https://www.googleapis.com/analytics/v3/management/accountSummaries';
-  private readonly accessToken = 'ya29.a0AfB_byBBs_28vAlQJVEcwkj8i9yl6hsu7jCVI946Kwpn0yDVKVapFq9YK0-WiUt8UheOGIKUyoZaBVUcguzoNeGtKMRnEwVcMteUfTRZX9-zzmvWK0fD691DMQ-mcYXMcNM4LWZUmhdOQ28YQZ9lZOddaCXHXi-cpLgUUxQaCgYKAX0SARESFQHsvYlsfT5e1SRFehoDdOp2peQxlw0174'; // Replace with your actual access token
 
   @Get('accountSummaries')
-  async getAccountSummaries() {
+  async getAccountSummaries(@Query('accessToken') paramValue: string) {
     try {
       const response = await axios.get(this.googleAnalyticsUrl, {
         headers: {
-          Authorization: `Bearer ${this.accessToken}`,
+          Authorization: `Bearer ${paramValue}`,
         },
       });
 
